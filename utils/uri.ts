9c28c4a2d9995fc8447b640061f25bb4156a9611
Austin S <acceptance364@gmail.com>
2025-01-26 14:52:52 -0800

    Updated prettier settings and pathUtil typing for potential external requirements.

--- utils/uri.ts
+++ utils/uri.ts
@@ -1,8 +1,16 @@
-/** Encode path params
+/** Encode path params.
+ * @warning Prefix is not enforced as tagged template literals cannot be completely type-safe at this time.
+ * 	The prefix is only for potential external type requirements.
  * @example pathUtil`/users/${id}` //->"/users/123"
  */
-const pathUtil = (parts: TemplateStringsArray, ...params: unknown[]): string =>
-	String.raw(parts, ...params.map((x) => encodeURIComponent(`${x}`)));
+const pathUtil = <TPrefix extends string = '/'>(
+	parts: TemplateStringsArray,
+	...params: Readonly<unknown>[]
+): `${TPrefix}${string}` =>
+	String.raw(
+		parts,
+		...params.map((x) => encodeURIComponent(`${x}`))
+	) as `${TPrefix}${string}`;
 
 /** Encode query params
  * @example queryUtil({
@@ -13,4 +21,4 @@ const pathUtil = (parts: TemplateStringsArray, ...params: unknown[]): string =>
 const queryUtil = (params: Record<string, string | number | boolean>) =>
 	Object.entries(params)
 		.map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
-		.join("&");
+		.join('&');
