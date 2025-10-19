ffa71bc8bf6d2d9365320b8a31d46f5590930790
acceptance364 <acceptance364@gmail.com>
2025-10-19 04:25:41 +0000

    working on api client utility

---
+++ utils/apiClient.ts
@@ -0,0 +1,36 @@
+type EndpointConfig<T, Meta extends Record<string, any>> = ({
+    baseUrl,
+    meta,
+}: {
+    baseUrl: string;
+    meta: Meta;
+}) => T;
+
+export function createApiClient<T, Meta extends Record<string, any> = {}>({
+    baseUrl: _baseUrl,
+    endpoints,
+    meta = {} as Meta,
+}: {
+    baseUrl: string;
+    endpoints: EndpointConfig<T, Meta>;
+    meta?: Meta;
+}){
+    const baseUrl = _baseUrl.endsWith('/') ? _baseUrl.slice(0, -1) : _baseUrl;
+    return endpoints({baseUrl, meta});
+}
+
+const client = createApiClient({
+    baseUrl: 'https://vpic.nhtsa.dot.gov/api/',
+    meta: {
+        apiToken: 'apple',
+    },
+    endpoints: ({baseUrl, meta}) => ({
+        decodeVin: async (vin: string) => {
+            const response = await fetch(`${baseUrl}/vehicles/decodevinvalues/${vin}?format=json`);
+            const json = await response.json();
+            return json.Results[0] as string;
+        }
+    }),
+});
+
+await client.decodeVin('1HGCM82633A123456');
