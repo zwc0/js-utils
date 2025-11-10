5f5c6f06a36a11082406646627065c14f17922ac
acceptance364 <acceptance364@gmail.com>
2025-11-10 03:52:30 +0000

    Defined header types and throw on fetch ok failure.

--- utils/api-client.test.ts
+++ utils/api-client.test.ts
@@ -1,17 +1,20 @@
-import { createApiClient } from "./api-client";
-import { uriUtil } from "./uri";
+import { createApiClient } from './api-client';
+import { uriUtil } from './uri';
 
 const client = createApiClient({
-    baseUrl: 'https://vpic.nhtsa.dot.gov/api/',
-    meta: {
-        apiToken: 'apple',
-    },
-    endpoints: ({api, meta}) => ({
-        decodeVin: async (vin: string) => {
-            const json = await api.get<any>(uriUtil`/vehicles/decodevinvalues/${vin}?format=json`);
-            return json.Results[0] as string;
-        },
-    }),
+	baseUrl: 'https://vpic.nhtsa.dot.gov/api/',
+	meta: {
+		apiToken: 'apple',
+	},
+	endpoints: ({ api, meta }) => ({
+		decodeVin: async (vin: string) => {
+			const res = await api.get(
+				uriUtil`/vehicles/decodevinvalues/${vin}?format=json`
+			);
+            const json = await res.json();
+			return json.Results[0] as string;
+		},
+	}),
 });
 
 await client.decodeVin('1HGCM82633A123456');
--- utils/api-client.ts
+++ utils/api-client.ts
@@ -1,47 +1,76 @@
-
 type Method = 'get' | 'post' | 'put' | 'delete';
 
-type FetchConfig = {
+type Headers = {
+	[key: string]: string;
+} & {
+	Authorization?: `Basic ${string}` | `Bearer ${string}` | (string & {});
+	'Content-Type'?:
+		| 'application/json'
+		| 'application/x-www-form-urlencoded'
+		| (string & {});
 };
 
-type ApiEndpoint = <TResponse>(endpoint: `/${string}`, options?: FetchConfig) => Promise<TResponse>;
+type FetchConfig = {
+	headers?: Headers;
+};
+
+type ApiEndpoint = (
+	endpoint: `/${string}`,
+	options?: FetchConfig
+) => Promise<Response>;
 
 type Api = Record<Method, ApiEndpoint>;
 
 type EndpointConfig<T, Meta extends Record<string, any>> = ({
-    baseUrl,
-    meta,
-    api,
+	baseUrl,
+	meta,
+	api,
 }: {
-    baseUrl: string;
-    meta: Meta;
-    api: Api;
+	baseUrl: string;
+	meta: Meta;
+	api: Api;
 }) => T;
 
 export function createApiClient<T, Meta extends Record<string, any> = {}>({
-    baseUrl: _baseUrl,
-    endpoints,
-    meta = {} as Meta,
+	baseUrl: _baseUrl,
+	endpoints,
+	meta = {} as Meta,
 }: {
-    baseUrl: string;
-    endpoints: EndpointConfig<T, Meta>;
-    meta?: Meta;
-}){
-    const baseUrl = _baseUrl.endsWith('/') ? _baseUrl.slice(0, -1) : _baseUrl;
+	baseUrl: string;
+	endpoints: EndpointConfig<T, Meta>;
+	meta?: Meta;
+}) {
+	const baseUrl = _baseUrl.endsWith('/') ? _baseUrl.slice(0, -1) : _baseUrl;
 
-    async function _fetch<TResponse>(method: Method, endpoint: `/${string}`, options: FetchConfig = {}){
-        const res = await fetch(`${baseUrl}${endpoint}`, options);
-        return await res.json() as TResponse;
-    }
+    /**
+     * Fetch wrapper
+     * @throws {Response} if response is not ok
+     */
+	async function _fetch(
+		method: Method,
+		endpoint: `/${string}`,
+		options: FetchConfig = {}
+	) {
+		const res = await fetch(`${baseUrl}${endpoint}`, {
+			method,
+			headers: {
+				'Content-Type': 'application/json',
+				...options.headers,
+			} satisfies Headers,
+		});
+        if (!res.ok)
+            throw res;
+		return res;
+	}
 
-    return endpoints({
-        baseUrl,
-        meta,
-        api: {
+	return endpoints({
+		baseUrl,
+		meta,
+		api: {
             delete: (endpoint, options) => _fetch('delete', endpoint, options),
-            get: (endpoint, options) => _fetch('get', endpoint, options),
-            post: (endpoint, options) => _fetch('post', endpoint, options),
-            put: (endpoint, options) => _fetch('put', endpoint, options),
-        },
-    });
+			get: (endpoint, options) => _fetch('get', endpoint, options),
+			post: (endpoint, options) => _fetch('post', endpoint, options),
+			put: (endpoint, options) => _fetch('put', endpoint, options),
+		},
+	});
 }
